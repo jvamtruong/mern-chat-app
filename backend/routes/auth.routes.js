e73f8c8041448eb554eb050e239dcc7c5cd25075
Nam Truong <jvamtruong@gmail.com>
2024-11-13 11:13:20 +0700

    dockerize app

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,87 @@
 import express from 'express'
-import { login, logout, signup, getCurrentUser } from '../controllers/auth.controller.js'
+import {
+  login,
+  logout,
+  signup,
+  getCurrentUser,
+} from '../controllers/auth.controller.js'
 import protectRoute from '../middleware/protectRoute.js'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /api/auth/signup:
+ *  post:
+ *    summary: signup
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                description: username
+ *              password:
+ *                type: string
+ *                description: password
+ *    responses:
+ *      201:
+ *
+ */
 router.post('/signup', signup)
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    summary: login
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                description: username
+ *              password:
+ *                type: string
+ *                description: password
+ *
+ *    responses:
+ *      200:
+ *        description: success
+ *      400:
+ *        description: invalid username or password
+ *      500:
+ *        description: server error
+ *
+ *
+ */
 router.post('/login', login)
+
+/**
+ * @swagger
+ * /api/auth/logout:
+ *  post:
+ *    summary: Logout current user
+ *    tags: [Auth]
+ *    responses:
+ *      200:
+ *        description: Successfully logged out
+ *      401:
+ *        description: Unauthorized - Invalid or missing JWT token
+ *      404:
+ *        description: User not found
+ *      500:
+ *        description: Server error
+ */
 router.post('/logout', protectRoute, logout)
 router.get('/me', protectRoute, getCurrentUser)
 
